5358fe78272fe7e1f6249b60610c5835a5bc0417
MCredbear <2363764471@qq.com>
2025-03-13 14:25:19 +0800

    first commit

---
+++ lib/utils.dart
@@ -0,0 +1,8 @@
+bool listEquals<T>(List<T>? list1, List<T>? list2) {
+  if (list1 == null || list2 == null) return list1 == list2;
+  if (list1.length != list2.length) return false;
+  for (int i = 0; i < list1.length; i++) {
+    if (list1[i] != list2[i]) return false;
+  }
+  return true;
+}
